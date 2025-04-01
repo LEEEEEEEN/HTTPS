b662d3d89bce768e3810b0dca3b6ced4e829d170
NuAsuka <nuasuka.mail@gmail.com>
2025-04-01 21:48:48 +0300

    change buttongs hours

--- add_habit.py
+++ add_habit.py
@@ -17,7 +17,7 @@ async def start_habit_creation(update: Update, context: ContextTypes.DEFAULT_TYP
 
 async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     habit_name = update.message.text
-    context.user_data['habit_name'] = habit_name
+    context.user_data['habit_name'] = habit_name #сохранение в временный словарь во время диалога
     
     # Создаем клавиатуру с выбором частоты
     keyboard = [
@@ -25,17 +25,17 @@ async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
         [InlineKeyboardButton("Раз в неделю", callback_data="раз в неделю")],
         [InlineKeyboardButton("Отмена", callback_data="отмена")],
     ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup = InlineKeyboardMarkup(keyboard) # создание клавиатуры (кнопок)
     
     await update.message.reply_text(f"Отлично! Теперь укажите, как часто вы хотите выполнять '{habit_name}':", reply_markup=reply_markup)
     return FREQUENCY
 
 
 async def handle_frequency(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
+    query = update.callback_query # получение данных и контекста о нажатой кнопке
     await query.answer()
     
-    if query.data == "cancel":
+    if query.data == "отмена":
         await query.edit_message_text("Создание привычки отменено.")
         return ConversationHandler.END
     
@@ -43,9 +43,19 @@ async def handle_frequency(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     # Создаем клавиатуру с выбором времени
     keyboard = [
-        [InlineKeyboardButton("08:00", callback_data="08:00"), InlineKeyboardButton("12:00", callback_data="12:00")],
-        [InlineKeyboardButton("16:00", callback_data="16:00"), InlineKeyboardButton("20:00", callback_data="20:00")],
-        [InlineKeyboardButton("Отмена", callback_data="cancel")],
+    [InlineKeyboardButton("00:00", callback_data="0"), InlineKeyboardButton("01:00", callback_data="1")],
+    [InlineKeyboardButton("02:00", callback_data="2"), InlineKeyboardButton("03:00", callback_data="3")],
+    [InlineKeyboardButton("04:00", callback_data="4"), InlineKeyboardButton("05:00", callback_data="5")],
+    [InlineKeyboardButton("06:00", callback_data="6"), InlineKeyboardButton("07:00", callback_data="7")],
+    [InlineKeyboardButton("08:00", callback_data="8"), InlineKeyboardButton("09:00", callback_data="9")],
+    [InlineKeyboardButton("10:00", callback_data="10"), InlineKeyboardButton("11:00", callback_data="11")],
+    [InlineKeyboardButton("12:00", callback_data="12"), InlineKeyboardButton("13:00", callback_data="13")],
+    [InlineKeyboardButton("14:00", callback_data="14"), InlineKeyboardButton("15:00", callback_data="15")],
+    [InlineKeyboardButton("16:00", callback_data="16"), InlineKeyboardButton("17:00", callback_data="17")],
+    [InlineKeyboardButton("18:00", callback_data="18"), InlineKeyboardButton("19:00", callback_data="19")],
+    [InlineKeyboardButton("20:00", callback_data="20"), InlineKeyboardButton("21:00", callback_data="21")],
+    [InlineKeyboardButton("22:00", callback_data="22"), InlineKeyboardButton("23:00", callback_data="23")],
+    [InlineKeyboardButton("Отмена", callback_data="cancel")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
