0c231e8fa87169176f6ea81b24a4dfa29f877b66
PRDGY <iprokhorov08@gmail.com>
2025-04-07 13:24:54 +0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from add_habit import add_habit, show_habits
 
@@ -9,20 +9,14 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Отправляем приветственное сообщение
     await update.message.reply_text(
-        "Привет! Я помогу тебе создать привычки. Я предлагаю две опции:\n"
-        "/add_habit — начать создание новой привычки\n"
-        "/my_habits — посмотреть список ваших привычек"
+        "Привет! Я помогу тебе создать привычки. Выберите одну из опций:",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("Создать привычку", callback_data="add_habit")],
+            [InlineKeyboardButton("Посмотреть мои привычки", callback_data="my_habits")]
+        ])
     )
-
-    # Автоматически отправляем команды, чтобы не нужно было их вводить вручную
-    await update.message.reply_text("Выберите действие:", reply_markup=InlineKeyboardMarkup([
-        [InlineKeyboardButton("Создать привычку", callback_data="/add_habit")],
-        [InlineKeyboardButton("Мои привычки", callback_data="/my_habits")]
-    ]))
 
 
 if __name__ == '__main__':
