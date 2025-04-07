89a3a2b87913079e87b13b876be84a39deaec5cf
PRDGY <iprokhorov08@gmail.com>
2025-04-07 13:27:12 +0300

    test

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
 from add_habit import add_habit, show_habits
-
 import logging
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "add_habit":
+        await start_habit_creation(update, context)
+    elif query.data == "my_habits":
+        await show_habits(update, context)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -22,7 +32,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     application = ApplicationBuilder().token("8100915495:AAFDv6ITyBPHY7pc7qKZuyWqkc_yG4BFkPQ").build()
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('my_habits', show_habits))
+    application.add_handler(CallbackQueryHandler(button_handler))
     add_habit(application)
 
     application.run_polling()
